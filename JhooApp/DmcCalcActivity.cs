ba5979c86f329dd074af6c78396289c68ac8019b
Mowstyl <indikalesken@hotmail.com>
2015-04-19 02:28:03 +0200

    Creado código para calcular daño.

--- JhooApp/DmcCalcActivity.cs
+++ JhooApp/DmcCalcActivity.cs
@@ -23,12 +23,136 @@ namespace JhooApp
 			SetContentView (Resource.Layout.DmgCalc);
 
 			Spinner sWeapType = FindViewById<Spinner> (Resource.Id.wType);
+			Button createWeapon = FindViewById<Button> (Resource.Id.start);
 
 			var adapter = ArrayAdapter.CreateFromResource (
 				this, Resource.Array.weapons_array, Android.Resource.Layout.SimpleSpinnerItem);
 
+			sWeapType.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs> (spinner_ItemSelected);
 			adapter.SetDropDownViewResource (Android.Resource.Layout.SimpleSpinnerDropDownItem);
 			sWeapType.Adapter = adapter;
+
+			createWeapon.Click += delegate {
+				calculateFunc();
+			};
+		}
+
+		public string aux;
+
+		public void calculateFunc()
+		{
+			EditText baseAttack = FindViewById<EditText> (Resource.Id.baseattack);
+			EditText elemAttack = FindViewById<EditText> (Resource.Id.elemattack);
+			EditText affinity = FindViewById<EditText> (Resource.Id.affinity);
+			EditText chaos = FindViewById<EditText> (Resource.Id.chaos);
+			CheckBox redSharp = FindViewById<CheckBox> (Resource.Id.redSharp);
+			CheckBox orangeSharp = FindViewById<CheckBox> (Resource.Id.orangeSharp);
+			CheckBox yellowSharp = FindViewById<CheckBox> (Resource.Id.yellowSharp);
+			CheckBox greenSharp = FindViewById<CheckBox> (Resource.Id.greenSharp);
+			CheckBox blueSharp = FindViewById<CheckBox> (Resource.Id.blueSharp);
+			CheckBox whiteSharp = FindViewById<CheckBox> (Resource.Id.whiteSharp);
+			CheckBox purpleSharp = FindViewById<CheckBox> (Resource.Id.purpleSharp);
+			TextView wPower = FindViewById<EditText> (Resource.Id.wPower);
+			TextView wEfPow = FindViewById<EditText> (Resource.Id.wEfPow);
+			TextView wElem = FindViewById<EditText> (Resource.Id.wElem);
+			TextView wEfPowS = FindViewById<EditText> (Resource.Id.wEfPowS);
+			TextView wElemS = FindViewById<EditText> (Resource.Id.wElemS);
+			int atk = int.Parse (baseAttack.Text);
+			double power = powerCalc(atk, aux);
+			int elem = int.Parse (elemAttack.Text);
+			int aff = int.Parse (affinity.Text);
+			float aMult;
+			if (aff >= 0)
+				aMult = 1.25f;
+			else
+				aMult = 0.75f;
+			float sBMult = 0, sEMult = 0;
+
+			if (purpleSharp.Checked){
+				sBMult=1.44f;
+				sEMult=1.2f;
+			}
+			else if (whiteSharp.Checked){
+				sBMult=1.32f;
+				sEMult=1.12f;
+			}
+			else if (blueSharp.Checked){
+				sBMult=1.20f;
+				sEMult=1.06f;
+			}
+			else if (greenSharp.Checked){
+				sBMult=1.05f;
+				sEMult=1.00f;
+			}
+			else if (yellowSharp.Checked){
+				sBMult=1.00f;
+				sEMult=0.75f;
+			}
+			else if (orangeSharp.Checked){
+				sBMult=0.75f;
+				sEMult=0.50f;
+			}
+			else if (redSharp.Checked){
+				sBMult=0.50f;
+				sEMult=0.25f;
+			}
+
+			wPower.Text = string.Format ("Poder: {0}", power);
+			wEfPow.Text = string.Format ("Poder efectivo: {0}", ((power*aMult)*aff+power*(100-aff))/100);
+			wElem.Text = string.Format ("Poder elemental: {0}", elem/10);
+			wEfPowS.Text = string.Format ("P. ef. + Filo Max: {0}", sBMult*((power*aMult)*aff+power*(100-aff))/100);
+			wElemS.Text = string.Format ("P. el. + Filo Max: {0}", sEMult*elem/10);
+		}
+
+		public void spinner_ItemSelected (Object sender, AdapterView.ItemSelectedEventArgs e)
+		{
+			Spinner spinner = (Spinner)sender;
+			aux = string.Format ("{0}", spinner.GetItemAtPosition (e.Position));
+
+		}
+
+		public double powerCalc (int bAtk, string wType)
+		{
+			float wMult = 1;
+			switch (wType) {
+			case "Gran espada":
+				wMult = 4.8f;
+				break;
+			case "Espada larga":
+				wMult = 3.3f;
+				break;
+			case "Espada y escudo":
+			case "Espadas dobles":
+				wMult = 1.4f;
+				break;
+			case "Martillo":
+			case "Cornamusa":
+				wMult = 5.2f;
+				break;
+			case "Lanza":
+			case "Lanza pistola":
+				wMult = 2.3f;
+				break;
+			case "Hacha espada":
+				wMult = 5.4f;
+				break;
+			case "Hacha cargada":
+				wMult = 3.6f;
+				break;
+			case "Glaive insecto":
+				wMult = 3.1f;
+				break;
+			case "Ballesta ligera":
+				wMult = 1.3f;
+				break;
+			case "Ballesta pesada":
+				wMult = 1.5f;
+				break;
+			case "Arco":
+				wMult = 1.2f;
+				break;
+			}
+			return bAtk / wMult;
 		}
 	}
 }
--- JhooApp/Resources/Resource.designer.cs
+++ JhooApp/Resources/Resource.designer.cs
@@ -82,53 +82,116 @@ namespace JhooApp
 			// aapt resource value: 0x7f060000
 			public const int MainLayout = 2131099648;
 			
-			// aapt resource value: 0x7f06000f
-			public const int button2 = 2131099663;
-			
-			// aapt resource value: 0x7f060010
-			public const int button3 = 2131099664;
-			
-			// aapt resource value: 0x7f06000e
-			public const int buttonCalc = 2131099662;
-			
 			// aapt resource value: 0x7f06000a
-			public const int buttonEquipment = 2131099658;
+			public const int affinity = 2131099658;
 			
-			// aapt resource value: 0x7f060007
-			public const int buttonItems = 2131099655;
+			// aapt resource value: 0x7f060006
+			public const int baseattack = 2131099654;
+			
+			// aapt resource value: 0x7f060013
+			public const int blueSharp = 2131099667;
+			
+			// aapt resource value: 0x7f060024
+			public const int button2 = 2131099684;
+			
+			// aapt resource value: 0x7f060025
+			public const int button3 = 2131099685;
+			
+			// aapt resource value: 0x7f060023
+			public const int buttonCalc = 2131099683;
+			
+			// aapt resource value: 0x7f060020
+			public const int buttonEquipment = 2131099680;
+			
+			// aapt resource value: 0x7f06001e
+			public const int buttonItems = 2131099678;
+			
+			// aapt resource value: 0x7f06001f
+			public const int buttonLocations = 2131099679;
+			
+			// aapt resource value: 0x7f06001c
+			public const int buttonMonsters = 2131099676;
+			
+			// aapt resource value: 0x7f06001d
+			public const int buttonQuests = 2131099677;
+			
+			// aapt resource value: 0x7f060021
+			public const int buttonSkills = 2131099681;
+			
+			// aapt resource value: 0x7f060022
+			public const int buttonUtils = 2131099682;
+			
+			// aapt resource value: 0x7f06000c
+			public const int chaos = 2131099660;
 			
 			// aapt resource value: 0x7f060008
-			public const int buttonLocations = 2131099656;
+			public const int elemattack = 2131099656;
 			
-			// aapt resource value: 0x7f060004
-			public const int buttonMonsters = 2131099652;
-			
-			// aapt resource value: 0x7f060005
-			public const int buttonQuests = 2131099653;
-			
-			// aapt resource value: 0x7f06000b
-			public const int buttonSkills = 2131099659;
-			
-			// aapt resource value: 0x7f06000d
-			public const int buttonUtils = 2131099661;
+			// aapt resource value: 0x7f060011
+			public const int greenSharp = 2131099665;
 			
 			// aapt resource value: 0x7f060001
 			public const int linearLayout1 = 2131099649;
 			
-			// aapt resource value: 0x7f060006
-			public const int linearLayout2 = 2131099654;
+			// aapt resource value: 0x7f060002
+			public const int linearLayout2 = 2131099650;
+			
+			// aapt resource value: 0x7f060005
+			public const int linearLayout3 = 2131099653;
+			
+			// aapt resource value: 0x7f060007
+			public const int linearLayout4 = 2131099655;
 			
 			// aapt resource value: 0x7f060009
-			public const int linearLayout3 = 2131099657;
+			public const int linearLayout5 = 2131099657;
 			
-			// aapt resource value: 0x7f06000c
-			public const int linearLayout4 = 2131099660;
+			// aapt resource value: 0x7f06000b
+			public const int linearLayout6 = 2131099659;
 			
-			// aapt resource value: 0x7f060002
-			public const int textView1 = 2131099650;
+			// aapt resource value: 0x7f06000d
+			public const int linearLayout7 = 2131099661;
+			
+			// aapt resource value: 0x7f060012
+			public const int linearLayout8 = 2131099666;
+			
+			// aapt resource value: 0x7f06000f
+			public const int orangeSharp = 2131099663;
+			
+			// aapt resource value: 0x7f060015
+			public const int purpleSharp = 2131099669;
+			
+			// aapt resource value: 0x7f06000e
+			public const int redSharp = 2131099662;
+			
+			// aapt resource value: 0x7f060016
+			public const int start = 2131099670;
 			
 			// aapt resource value: 0x7f060003
-			public const int wType = 2131099651;
+			public const int textView1 = 2131099651;
+			
+			// aapt resource value: 0x7f060018
+			public const int wEfPow = 2131099672;
+			
+			// aapt resource value: 0x7f06001a
+			public const int wEfPowS = 2131099674;
+			
+			// aapt resource value: 0x7f060019
+			public const int wElem = 2131099673;
+			
+			// aapt resource value: 0x7f06001b
+			public const int wElemS = 2131099675;
+			
+			// aapt resource value: 0x7f060017
+			public const int wPower = 2131099671;
+			
+			// aapt resource value: 0x7f060004
+			public const int wType = 2131099652;
+			
+			// aapt resource value: 0x7f060014
+			public const int whiteSharp = 2131099668;
+			
+			// aapt resource value: 0x7f060010
+			public const int yellowSharp = 2131099664;
 			
 			static Id()
 			{
